78acaed0e8be054c84874484fec490526da768e4
Dixita_0568 <dixitamishra23@gmail.com>
2025-08-11 11:02:33 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,16 @@
+public class Variable{
+    public static void main(String arg[]){
+        //Pov:when you discovered cases other than upper and lower (camel case  <(* - *)>)
+        String aliaBhatt="Disco Diwaane";
+        int $bills=999999999;
+        String _money=" $$ Paisa hi paisa $$ ";
+        System.out.println(aliaBhatt+_money+$bills);
+        //Teen tarike se sum
+        int num1=190705;//local variable
+        int num2=231106;
+        int sum=num1+num2;
+        System.out.println(sum);
+        //method 2
+        System.out.println(num1+num2);
+    }
+}
