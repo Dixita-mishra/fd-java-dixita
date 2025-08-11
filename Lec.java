82528f55b1e2700b9b4ee35e1f64d89bebfde569
Dixita_0568 <dixitamishra23@gmail.com>
2025-08-11 10:15:56 +0530

    Add files via upload

---
+++ Lec.java
@@ -0,0 +1,10 @@
+public class Lec {
+    public static void main(String arg[]){
+        System.out.println("Hello");
+        System.out.println("Dixita");
+        /*This is a multiline comment...
+          You can enter comment in multiple lines */
+        // This is a single line comment..ohh yeah!!
+
+    }
+}  
