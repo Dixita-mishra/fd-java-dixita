4a43c0c011c67f2de1beef5b8e42aaaefeb864b2
Dixita_0568 <dixitamishra23@gmail.com>
2025-08-12 08:31:01 +0530

    Add files via upload

---
+++ Datatypel.java
@@ -0,0 +1,10 @@
+//Non- primitive datatype
+public class Datatypel{
+    public static void main(String args[]){
+        String name="Sheela";
+        int marks[]={24,25,24,23,22};
+        System.out.println(name +": ");
+        System.out.println(marks[0]);
+        System.out.println(marks[4]);
+    }
+}
