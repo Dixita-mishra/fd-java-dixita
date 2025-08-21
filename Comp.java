5da9c205ca6cf423a8bf186061ff58e53115815f
Dixita-mishra <dixitamishra23@gmail.com>
2025-08-21 13:51:29 +0530

    Add files via upload

---
+++ Comp.java
@@ -0,0 +1,13 @@
+public class Comp {
+    public static void main(String[] args) {
+        //comparison operater
+        int x = 5;
+        int y = 10;
+        System.out.println("x==y"+ (x == y));
+        System.out.println("x!=y"+ (x != y));
+        System.out.println("x>y"+ (x > y));
+        System.out.println("x<y"+ (x < y));
+        System.out.println("x>=y"+ (x >= y));
+        System.out.println("x<=y"+ (x <= y));
+        
+    }}
