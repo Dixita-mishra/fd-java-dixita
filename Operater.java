e93e8110b8781d8043abde77c9e23186ff57b82f
Dixita-mishra <dixitamishra23@gmail.com>
2025-08-21 13:42:12 +0530

    Add files via upload

---
+++ Operater.java
@@ -0,0 +1,17 @@
+public class Operater{
+    public static void main(String[] args) {
+        //arithmetic operater
+        int a= 10;
+        int b= 20;
+        int sum = a + b;
+        int prod=a*b;
+        int div=a/b;
+        int diff=a-b;
+        int mod=a%b;
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + prod);
+        System.out.println("Division: " + div);
+        System.out.println("Difference: " + diff);
+        System.out.println("Modulus: " + mod);
+}
+}
