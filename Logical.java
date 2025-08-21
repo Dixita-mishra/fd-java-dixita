f37e1cf2c4d562af8dcaa411c6a449ab05791554
Dixita-mishra <dixitamishra23@gmail.com>
2025-08-21 13:51:04 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,11 @@
+public class Logical {
+    public static void main(String[] args) {
+        // logical operators
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("a && b = " + (a && b)); // logical AND
+        System.out.println("a || b = " + (a || b)); // logical OR
+        System.out.println("!a = " + (!a)); // logical NOT
+    }
+}
