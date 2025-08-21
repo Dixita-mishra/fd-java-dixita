a9627b865f4406bc97f602d15fc084fcaa0baee1
Dixita-mishra <dixitamishra23@gmail.com>
2025-08-21 13:44:40 +0530

    Add files via upload

---
+++ Assignment_op.java
@@ -0,0 +1,20 @@
+public class Assignment_op {
+    public static void main(String[] args) {
+         //assignment operater
+        int x = 5;
+        x += 3; 
+        System.out.println("After += operation, x = " + x);
+        x -= 2;
+        System.out.println("After -= operation, x = " + x);
+        x *= 2;
+        System.out.println("After *= operation, x = " + x);
+        x /= 4;
+        System.out.println("After /= operation, x = " + x);
+        x %= 3;
+        System.out.println("After %= operation, x = " + x);
+    }}
+    
+
+
+        
+
