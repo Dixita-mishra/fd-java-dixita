cb451c2b59908e3bb16d4cddd98d40194754bd0e
Dixita-mishra <dixitamishra23@gmail.com>
2025-08-21 13:56:14 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,21 @@
+public class Unary {
+    public static void main(String[] args) {
+        // unary operators
+        int a = 5;
+        System.out.println("a = " + a);
+        
+        // unary plus
+        System.out.println("+a = " + (+a));
+        
+        // unary minus
+        System.out.println("-a = " + (-a));
+        
+        // increment operator
+        a++;
+        System.out.println("After increment, a = " + a);
+        
+        // decrement operator
+        a--;
+        System.out.println("After decrement, a = " + a);
+    }
+}
